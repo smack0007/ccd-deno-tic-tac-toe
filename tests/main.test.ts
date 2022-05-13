a69a6468692194a90784d5b1290790c917d4388f
zachary.snow <zachary.snow@conciso.de>
2022-05-13 14:15:46 +0200

    Remove main.test.ts

--- tests/main.test.ts
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "./_deps.ts";
-import { greet } from "../src/main.ts";
-
-Deno.test("greet should return expected string", () => {
-  assertEquals(greet(), "Hello World!");
-});
